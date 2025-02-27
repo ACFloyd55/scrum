6e5ff20192f92ae0c0e73a4621b335971b7b0863
Cooper Floyd <afloyd@student.neumont.edu>
2025-02-27 10:58:15 -0700

    Add main method to Main class

--- Scrum_Final/src/Main.java
+++ Scrum_Final/src/Main.java
@@ -1,7 +1,7 @@
 package Controller.Scrum_Final.src;
 
 public class Main {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         System.out.println("Hello, Scrum World!");
     }
 }
